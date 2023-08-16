aece1970b3e6d40bbffa2557f28a169120145051
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-08-16 22:46:22 +0200

    feat: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,34 @@
 export interface RootLayoutProps {
   children: React.ReactNode;
 }
+
+//Form messages
+export interface IMessage {
+  sender: 'admin' | 'user';
+  text: string;
+}
+
+export interface IUserData {
+  name: string;
+  reason: string;
+  phone: string;
+  email: string;
+}
+
+export interface IQuestionAction {
+  question: string | string[];
+  action: (message: string, userData: IUserData) => IUserData;
+}
+
+//Music
+export interface Track {
+  preview_url: string | null;
+  album: { images: [{ url: string }]; name: string };
+  name: string;
+  artists: [{ name: string }];
+  explicit: boolean;
+}
+
+export interface PlaylistItem {
+  track: Track;
+}
