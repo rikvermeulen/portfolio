187714b7f24b7c67015c6a9e68be40f0f9d451e0
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-08-18 17:44:28 +0200

    feat: podcast module

---
+++ src/components/Bento/types/Podcast.tsx
@@ -0,0 +1,153 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+import cc from '@/lib/cc';
+import AudioPlayer from '@/components/AudioPlayer';
+import Icon from '@/components/Icons/Icon';
+
+import Bento from '../Bento';
+
+interface PodcastProps {
+  playlist: PodcastItem[];
+  className?: string;
+}
+
+export interface PodcastItem {
+  audio_preview_url: string;
+  images: [{ url: string }];
+  name: string;
+  explicit: boolean;
+}
+
+const Podcast: React.FC<PodcastProps> = ({ playlist = [], className }) => {
+  const [playlistTracks, setPlaylistTracks] = useState<PodcastItem[]>([]);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const {
+    audioRef,
+    isPlaying,
+    previewUrl,
+    setPreviewUrl,
+    playOrPause,
+    handleVolumeChange,
+    changeTrack,
+    handleTimeUpdate,
+  } = AudioPlayer({
+    initialUrl: '',
+    playlistTracks: playlistTracks,
+    currentTrackIndex,
+    setCurrentTrackIndex,
+  });
+
+  useEffect(() => {
+    const tracksWithPreview = playlist.filter((track) => !!track.audio_preview_url);
+    setPlaylistTracks(tracksWithPreview);
+    console.log(tracksWithPreview[0]);
+    const initialPreviewUrl = tracksWithPreview[0]?.audio_preview_url;
+    if (initialPreviewUrl) {
+      setPreviewUrl(initialPreviewUrl);
+    }
+  }, [playlist]);
+
+  const currentTrack = playlistTracks[currentTrackIndex];
+  const image = currentTrack?.images[0]?.url || '/images/noalbum.png';
+
+  return (
+    <Bento size="1x1" className={cc(className, isPlaying && '', 'bento relative z-0 !border-none')}>
+      <div className="absolute right-0 z-20 p-5">
+        <a href="https://music.apple.com/nl/playlist/r-b/pl.u-38oWX9ECd2XAl3" target="_blank">
+          <Image
+            src="/images/icons/podcast.png"
+            className={cc(
+              isPlaying && ' bg-gradient-to-b from-[#BC6AEB] to-[#6E2AAD] ',
+              'rounded-md p-1.5 drop-shadow-md transition-transform duration-300 hover:scale-105',
+            )}
+            alt="media"
+            width={32}
+            height={32}
+          />
+        </a>
+      </div>
+      <div
+        className={`absolute z-0 h-full w-full bg-cover transition-opacity duration-500 ease-in-out ${
+          isPlaying ? 'opacity-100' : 'opacity-0'
+        }`}
+        style={{
+          backgroundImage: image ? `url(${image})` : '',
+        }}
+      >
+        <div className=" h-full w-full backdrop-blur-xl"></div>
+      </div>
+      <div className={'relative z-10 h-full p-5'}>
+        <div className="flex h-40 w-full items-center justify-center">
+          {image && (
+            <Image
+              src={image}
+              priority
+              alt={currentTrack?.name || 'No album'}
+              width={144}
+              height={144}
+              className={cc(
+                isPlaying ? 'pop-up' : 'pop-down',
+                'rounded-md drop-shadow-md duration-300 transition-transform scale-100',
+              )}
+            />
+          )}
+        </div>
+        <div className="mt-6 flex flex-col text-sm text-white">
+          <div className="flex items-center gap-2">
+            <p className="font-bold">{currentTrack?.name}</p>
+            {currentTrack?.explicit && <Icon type="explicit" className="w-2.5" />}
+          </div>
+          <div className="relative -top-1 text-white/70">Podcast</div>
+        </div>
+        <div className="m-auto mt-4 flex w-full max-w-[184px] items-center justify-between">
+          <button
+            onClick={() => changeTrack('previous')}
+            className="w-5 fill-white"
+            name="Previous song"
+          >
+            <Icon type="next" />
+          </button>
+          <button
+            onClick={playOrPause}
+            className="flex h-6 w-6 justify-center fill-white"
+            name="Play / Pause"
+          >
+            {isPlaying ? <Icon type="pause" className="w-5" /> : <Icon type="play" />}
+          </button>
+          <button
+            onClick={() => changeTrack('next')}
+            className="w-5 -scale-x-100 fill-white active:animate-ping"
+            name="Next song"
+          >
+            <Icon type="next" />
+          </button>
+        </div>
+        <label className="mt-6 flex items-center justify-center gap-4" htmlFor="range">
+          <Icon type="mute" className="w-1.5 fill-white" />
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            defaultValue="0.5"
+            id="range"
+            name="range"
+            onChange={handleVolumeChange}
+            className="slider"
+            style={{ '--background-size': '50%' } as React.CSSProperties}
+          />
+          <Icon type="sound" className="w-3 fill-white" />
+        </label>
+        <audio ref={audioRef} style={{ display: 'none' }} onTimeUpdate={handleTimeUpdate}>
+          <source src={previewUrl || ''} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </Bento>
+  );
+};
+
+export default Podcast;
