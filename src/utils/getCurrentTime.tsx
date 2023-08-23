8e138f12d43034ebc06be82b162aba382328b6d1
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-08-23 13:02:14 +0200

    fix: get currenttime stamp without hydration err

--- src/utils/getCurrentTime.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export function getCurrentTime() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const currentTime = `${hours}:${minutes}`;
-  return currentTime;
-}
