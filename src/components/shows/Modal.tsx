5e761410a258b76d5ea06bf552672e95672c8d97
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-08-25 11:12:33 +0200

    perf: small styling edites

--- src/components/shows/Modal.tsx
+++ src/components/shows/Modal.tsx
@@ -23,26 +23,31 @@ export default function Modal({
   return (
     <div
       className={cc(
-        isVisible ? 'opacity-100 scale-100' : ' opacity-0 scale-0',
-        'absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black/10 backdrop-blur-lg px-5 transition-[opacity,transform] duration-300',
+        isVisible ? 'opacity-100' : ' opacity-0 -z-10',
+        'absolute left-0 top-0 flex h-full w-full items-center justify-center bg-[#828282]/40 px-5 backdrop-blur-lg transition-[opacity,transform] duration-300',
       )}
     >
-      <div className="rounded-xl bg-[#1C1C1E] p-4">
+      <div
+        className={cc(
+          isVisible ? 'scale-100' : ' scale-0',
+          'rounded-xl bg-black/60 p-4 backdrop-blur-lg',
+        )}
+      >
         <div className="flex justify-between">
-          <h2 className="mb-4 text-lg font-bold text-white">Suggestions 🎬</h2>
+          <h2 className="mb-4 text-base font-bold text-white">Your suggestions 🍿</h2>
           <Icon
             type="close"
             className="h-6 w-6 cursor-pointer rounded-full bg-[#4E5152] fill-white p-2 transition-colors duration-300 hover:bg-[#3d4040]"
             onClick={toggleModal}
           />
         </div>
-        <div className="mb-4 rounded-lg bg-[#282c2d] px-3 py-2">
+        <div className="mb-4 rounded-lg bg-[#373b3c] px-3 py-2">
           <input
             type="email"
             placeholder="Email"
             value={formData.email}
             onChange={(e) => setFormData((prev: any) => ({ ...prev, email: e.target.value }))}
-            className="w-full  border-b-[0.2px] border-solid border-[#4E5152] bg-transparent px-1 pb-2 pt-1  text-base text-white outline-none sm:text-sm"
+            className="w-full  rounded-none border-b-[0.2px] border-solid border-[#4E5152] bg-transparent px-1 pb-2 pt-1 text-base text-white outline-none sm:text-sm"
           />
           <input
             placeholder="Your recommendation"
@@ -51,7 +56,7 @@ export default function Modal({
             onChange={(e) =>
               setFormData((prev: any) => ({ ...prev, recommendation: e.target.value }))
             }
-            className="w-full resize-none border-none bg-transparent px-1 pb-1 pt-2 text-base text-white outline-none sm:text-sm"
+            className="w-full resize-none rounded-none border-none bg-transparent px-1 pb-1 pt-2 text-base text-white outline-none sm:text-sm"
           ></input>
         </div>
         <div className="flex justify-between">
