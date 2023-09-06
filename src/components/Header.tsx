86f8153bd8131672c5da7fcb27ba04a4264bed42
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-06 21:32:32 +0200

    test: new version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,8 +23,8 @@ export default function Header() {
           <Image src={'/images/logo.png'} alt="logo" width="20" height="20" />
           <p className="text-xs font-medium sm:text-sm">Rik Vermeulen</p>
         </Link>
-        <div className="flex items-center justify-center gap-1 rounded-full border-solid p-1 text-[#707070] backdrop-blur-lg duration-300 xl:border  xl:border-[#DEDEDE] xl:bg-primary/60 xl:shadow-sm">
-          <p className="hidden pl-2 text-sm font-medium">{time || '00:00'}</p>
+        <div className="flex items-center justify-center gap-1 rounded-full border border-solid border-[#DEDEDE] bg-primary/60 p-1 text-black  backdrop-blur-lg duration-300 xl:shadow-sm">
+          <p className="pl-3 text-xs">{time + ' PM' || '00:00'}</p>
           <div
             className={cc(
               musicData.isPlaying ? '' : 'hidden',
@@ -35,18 +35,18 @@ export default function Header() {
               type="wave"
               className={cc(
                 musicData.isPlaying ? 'scale-100' : 'scale-0',
-                'h-auto w-3 fill-[#707070] hover:fill-white transition-[transform,width] duration-300 delay-300',
+                'h-auto w-3 fill-black hover:fill-white transition-[transform,width] duration-300 delay-300',
               )}
             />
           </div>
-          <div className="group hidden rounded-full p-2 transition-colors hover:bg-white hover:shadow-2xl">
+          <div className="group rounded-full p-2 transition-colors hover:bg-white hover:shadow-2xl">
             <Icon
               type="settings"
-              className="h-auto w-3.5 cursor-pointer fill-[#707070] transition-transform hover:fill-white group-hover:scale-110 group-hover:fill-black"
+              className="h-auto w-3 cursor-pointer fill-black transition-transform hover:fill-white group-hover:scale-110 group-hover:fill-black"
             />
           </div>
           <Image
-            src={'/images/profile.png'}
+            src={'/images/me.jpg'}
             className=" rounded-full shadow-md"
             alt="profile"
             width="32"
