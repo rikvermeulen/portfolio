4c21ff495b26f8d294b9be9f207de63dc56d8c10
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-11 22:10:59 +0200

    fix: removed errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
-import { withContentlayer } from 'next-contentlayer';
-
 import './src/env.mjs';
+
+import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'error',
+    };
+
+    return config;
+  },
   swcMinify: true,
   images: {
     remotePatterns: [
@@ -24,5 +31,4 @@ const config = {
     ],
   },
 };
-
 export default withContentlayer(config);
