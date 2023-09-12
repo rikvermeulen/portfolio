a6ffbd642b47a678a52519946ba9caf7280a7ad9
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-12 11:20:22 +0200

    fix: switch to locale time

--- src/utils/useFormattedDate.tsx
+++ src/utils/useFormattedDate.tsx
@@ -1,9 +1,7 @@
 import { DateTime } from 'luxon';
 
 const getFormattedDate = (date: string) => {
-  const localTime = DateTime.fromISO(date, { zone: 'utc' })
-    .setZone('Europe/Amsterdam')
-    .toFormat('h:mm');
+  const localTime = DateTime.fromISO(date, { zone: 'utc' }).toLocal().toFormat('h:mm');
 
   return localTime;
 };
