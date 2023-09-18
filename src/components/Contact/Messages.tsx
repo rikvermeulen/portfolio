f7f178a06a13177b03ed519f6955363cc83fb97b
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-18 17:07:46 +0200

    fix: reverted change of styling messages

--- src/components/Contact/Messages.tsx
+++ src/components/Contact/Messages.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { IMessage } from '@/types';
@@ -12,27 +14,25 @@ export const AdminMessage = ({
 }: {
   message: IMessage;
   isSameSender: boolean;
-}) => {
-  const bubbleClassNames = cc(
-    isSameSender && 'ml-10 md:ml-12 relative -top-3',
-    'relative w-fit max-w-[220px] rounded-3xl bg-[#E9E9EB] px-3 py-2 md:max-w-xs',
-  );
-
-  return (
-    <div className="flex w-full gap-2">
-      {!isSameSender && (
-        <>
-          <Image
-            src="/images/profile.png"
-            alt="Me"
-            width={40}
-            height={40}
-            className="mt-4 w-8 self-start rounded-full md:w-10"
-          />
-          <p className="ml-3 text-xs text-[#8D8D93]">Rik</p>
-        </>
-      )}
-      <div className={bubbleClassNames}>
+}) => (
+  <div className="flex w-full gap-2">
+    {!isSameSender && (
+      <Image
+        src="/images/profile.png"
+        alt="Me"
+        width={40}
+        height={40}
+        className="mt-4 w-8 self-start rounded-full md:w-10"
+      />
+    )}
+    <div>
+      {!isSameSender && <p className="ml-3 text-xs text-[#8D8D93]">Rik</p>}
+      <div
+        className={cc(
+          isSameSender && 'ml-10 md:ml-12 relative -top-3',
+          'relative w-fit max-w-[220px] rounded-3xl bg-[#E9E9EB] px-3 py-2 md:max-w-xs',
+        )}
+      >
         {!isSameSender && (
           <Icon
             type="tail"
@@ -42,8 +42,8 @@ export const AdminMessage = ({
         {message.text}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export const UserMessage = ({ message, index }: { message: IMessage; index: number }) => {
   const [isDelivered, setIsDelivered] = useState(false);
@@ -57,18 +57,20 @@ export const UserMessage = ({ message, index }: { message: IMessage; index: numb
     return () => clearTimeout(timer);
   }, [index]);
 
-  const deliveryClassNames = cc(
-    isDelivered ? 'opacity-100 scale-100' : 'opacity-0 scale-50',
-    'mt-1 self-end text-xs text-gray-400 transition-[opacity,transform] duration-200',
-  );
-
   return (
     <div className="flex w-full flex-col">
       <div className="relative w-fit max-w-xs self-end rounded-3xl bg-[#007AFF] px-3 py-2 text-white">
         <Icon type="tail" className="absolute -right-1 bottom-0.5 h-4 w-4 fill-[#007AFF]" />
         {message.text}
       </div>
-      <div className={deliveryClassNames}>Delivered</div>
+      <div
+        className={cc(
+          isDelivered ? 'opacity-100 scale-100' : 'opacity-0 scale-50',
+          'mt-1 self-end text-xs text-gray-400 transition-[opacity,transform] duration-200',
+        )}
+      >
+        Delivered
+      </div>
     </div>
   );
 };
